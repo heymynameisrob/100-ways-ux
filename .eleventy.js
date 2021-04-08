4e5b6e859f20f9c55c777078f51d415bc54e3748
Rob Hough <robhough180@gmail.com>
2021-04-08 17:54:10 +0100

    90% built

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./_tmp/style.css')
 
   eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+  eleventyConfig.addPassthroughCopy('./images')
 
   eleventyConfig.addShortcode('version', function () {
     return now
--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: {
     content: ['_site/**/*.html'],
@@ -6,11 +8,21 @@ module.exports = {
     },
   },
   theme: {
-    extend: {
-      colors: {
-        change: 'transparent',
-      },
+    fontFamily: {
+      'sans': 'DM Sans, sans-serif',
     },
+    colors: {
+      ...colors,
+      "primary": '#8E64FF',
+      "primary-dark": '#5b34c2',
+      "primary-light": "#e2d8fe",
+      "muted": "#FCFAFA"
+    },
+    extend: {
+      zIndex: {
+        '-1': '-1'
+      }
+    }
   },
   variants: {},
   plugins: [],
